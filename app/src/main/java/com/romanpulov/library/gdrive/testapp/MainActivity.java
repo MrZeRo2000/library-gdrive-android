f7525a1e1dd98e634a183387b93c44121871e73b
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-08-30 23:46:09 +0300

    first sign in

--- app/src/main/java/com/romanpulov/library/gdrive/testapp/MainActivity.java
+++ app/src/main/java/com/romanpulov/library/gdrive/testapp/MainActivity.java
@@ -1,14 +1,72 @@
 package com.romanpulov.library.gdrive.testapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private static final int REQ_ONE_TAP = 2;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signInButton = findViewById(R.id.button_sign_in);
+        signInButton.setOnClickListener(v -> {
+            oneTapClient = Identity.getSignInClient(this);
+            signInRequest = BeginSignInRequest.builder()
+                    .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                            .setSupported(true)
+                            .build())
+                    .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                            .setSupported(true)
+                            // Your server's client ID, not your Android client ID.
+                            .setServerClientId(getString(R.string.default_web_client_id))
+                            // Only show accounts previously used to sign in.
+                            .setFilterByAuthorizedAccounts(false)
+                            .build())
+                    // Automatically sign in when exactly one credential is retrieved.
+                    .setAutoSelectEnabled(true)
+                    .build();
+
+            oneTapClient.beginSignIn(signInRequest)
+                    .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
+                        @Override
+                        public void onSuccess(BeginSignInResult result) {
+                            try {
+                                startIntentSenderForResult(
+                                        result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
+                                        null, 0, 0, 0);
+                            } catch (IntentSender.SendIntentException e) {
+                                Log.e(TAG, "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                            }
+                        }
+                    })
+                    .addOnFailureListener(this, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // No saved credentials found. Launch the One Tap sign-up flow, or
+                            // do nothing and continue presenting the signed-out UI.
+                            Log.d(TAG, e.getLocalizedMessage());
+                        }
+                    });
+        });
+
     }
 }
